1342180455f9db4f53489909f3910f08ee0cd0b8
Stef <stefvdburgt>
2019-11-13 12:54:08 +0100

    Added functionality to the burger menu button

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ function test() {
 function smartScroll(parameter) {
   var x = parameter;
   document.getElementById(x).scrollIntoView({ block: 'end',  behavior: 'smooth' });
+  var x = document.getElementById("wrapperStickyMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
